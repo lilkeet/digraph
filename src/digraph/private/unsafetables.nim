0a0f2cd4b4e26d2a37faae7ac2cbbfc968de26b6
lilkeet <kitorito13@gmail.com>
2024-11-09 03:40:00 -0600

    more unsafe table functionality

--- src/digraph/private/unsafetables.nim
+++ src/digraph/private/unsafetables.nim
@@ -8,6 +8,10 @@ import
   std / [tables {.all.}, hashes, importutils],
   ./debugtools
 
+# import hashcommon
+
+# discard rawget
+
 {.hint[Performance]:on.}
 {.experimental: "strictFuncs".}
 when defined(nimHasStrictDefs):
@@ -17,6 +21,33 @@ when defined(nimHasStrictDefs):
 type TableIndex*[A, B] = distinct int
   ## An index to a location in a table.
 
+func `<`(a, b: TableIndex): bool {.borrow.}
+
+func myRawGet[A, B](t: Table[A, B]; key: A; hc: out Hash): TableIndex[A, B] {.inline.} =
+  hc = hash(key)
+  if hc == 0: # This almost never taken branch should be very predictable.
+    when sizeof(int) < 4:
+      hc = 31415 # Value doesn't matter; Any non-zero favorite is fine <= 16-bit.
+    else:
+      hc = 314159265 # Value doesn't matter; Any non-zero favorite is fine.
+
+  privateAccess Table
+  debugAssert t.dataLen != 0, "Table construction defect."
+  # if t.dataLen == 0:
+  #   return -1
+  var h: Hash = hc and maxHash(t) # start with real hash value
+  while isFilled(t.data[h].hcode):
+    # Compare hc THEN key with boolean short circuit. This makes the common case
+    # zero ==key's for missing (e.g.inserts) and exactly one ==key for present.
+    # It does slow down succeeding lookups by one extra Hash cmp&and..usually
+    # just a few clock cycles, generally worth it for any non-integer-like A.
+    if t.data[h].hcode == hc and t.data[h].key == key:
+      return TableIndex[A, B](h)
+    h = nextTry(h, maxHash(t))
+  result = TableIndex[A, B](-1 - h) # < 0 => MISSING; insert idx = -1 - result
+
+
+
 func getIndex*[A; B](
     table: Table[A, B]; key: sink A
   ): TableIndex[A, B] {.raises: [].} =
@@ -24,7 +55,7 @@ func getIndex*[A; B](
   ## Checks are only made in debug builds.
   debugAssert key in table, "Key defect."
   var hc: Hash
-  result = TableIndex[A, B](rawGet(table, key, hc))
+  result = myRawGet(table, key, hc)
 
 
 # type KeyValuePair*[A, B] = tuple[hcode: Hash, key: A, val: B]
@@ -68,3 +99,37 @@ func unsafeGet*[A; B](
   ## Undefined behavior when `key notin table`.
   let index = getIndex(table, key)
   result = table.getValue(index)
+
+
+
+
+func myRawInsert[A, B](
+    t: var Table[A, B]; theKey: sink A; newVal: sink B; hc: sink Hash;
+    h: sink TableIndex[A, B]
+  ) {.inline.} =
+  privateAccess Table
+  t.data[int h].key = theKey
+  t.data[int h].val = newVal
+  t.data[int h].hcode = hc
+  inc t.counter
+
+func defaultOrInc*[A; B: Ordinal](t: var Table[A, B], key: sink A) =
+  ## If the table does not have a value stored at `key`, the default value
+  ## is put there.
+  ## If it does have a value there, it is incremented.
+  ##
+  ## This proc assumes that your table is already at a good size!
+  # privateAccess Table
+  var hc: Hash
+  var index = myRawGet(t, key, hc)
+  if index < TableIndex[A, B](0):
+    debugAssert not mustRehash(t), "Table construction defect."
+    # if mustRehash(t):
+    #   enlarge(t)
+    #   index = rawGetKnownHC(t, key, hc)
+    index = TableIndex[A, B](-1 - int(index)) # important to transform for mgetOrPutImpl
+    t.myRawInsert(key, default(B), hc, index)
+    # inc t.counter
+  else:
+    inc t.getValue(index)
+
